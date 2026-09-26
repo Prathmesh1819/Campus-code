c5d4f675abfb3b1968dfb967a06e6f6eff8f3fa6
Prathmesh Dharashivkar <prathmeshdharashivkar18@gmail.com>
2026-09-26 17:36:50 +0530

    fix(next.config): add jwks-rsa and jose to serverExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["firebase-admin"],
+  serverExternalPackages: ["firebase-admin", "jwks-rsa", "jose"],
 };
 
 module.exports = nextConfig;
